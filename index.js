8706b56bb46198d08d6f0f2e5051e736b13d76b5
Peter Kwok <peter.kwok94@gmail.com>
2024-02-28 00:00:54 -0500

    Initial commit, middleware + req.body test

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+// my code
+const bodyParser = require('body-parser')
+// 
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,9 +12,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// my code
+app.use("/api/users", bodyParser.urlencoded())
+app.use("/api/users", bodyParser.json())
 
+app.post("/api/users", (req, res) => {
 
+})
 
+//
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
