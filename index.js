3bdfc314a6cdc1d5a0951420d0f8b285a60d9123
Peter Kwok <peter.kwok94@gmail.com>
2024-02-29 03:45:21 -0500

    feat: save users

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 require('dotenv').config()
 // my code
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 // 
 
 app.use(cors())
@@ -13,10 +14,29 @@ app.get('/', (req, res) => {
 });
 
 // my code
-app.use("/api/users", bodyParser.urlencoded())
+app.use("/api/users", bodyParser.urlencoded({ extended: true }))
 app.use("/api/users", bodyParser.json())
 
+mongoose.connect(process.env.MONGODB_URI)
+
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  username: String
+})
+const user = mongoose.model('User', userSchema)
+
 app.post("/api/users", (req, res) => {
+  const userToSave = user({ username: req.body.username })
+  userToSave.save()
+    .then((savedUser) => {
+      res.json(savedUser)
+    })
+    .catch((err) => {
+      res.send(err)
+    })
+})
+
+app.get("/api/users", (req, res) = {
 
 })
 
